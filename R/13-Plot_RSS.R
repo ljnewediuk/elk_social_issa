758261453adf8a99d80da7d3cfdb21b0f2fd329c
ljnewediuk <j.newedi@gmail.com>
2025-12-24 12:30:02 -0330

    add sri rss

--- R/13-Plot_RSS.R
+++ R/13-Plot_RSS.R
@@ -16,19 +16,23 @@ rss_id_NNdist <- readRDS('rss/rss_id_NNdist.rds')
 rss_pop_NNdist <- readRDS('rss/rss_pop_NNdist.rds')
 rss_id_wang <- readRDS('rss/rss_id_wang.rds')
 rss_pop_wang <- readRDS('rss/rss_pop_wang.rds')
+rss_id_sri <- readRDS('rss/rss_id_sri.rds')
+rss_pop_sri <- readRDS('rss/rss_pop_sri.rds')
 
 ## 2- Unscale and uncentre variables ====
 
 # Variable means
 mu_NNdist <- mean(dat$lStartDist, na.rm = T)
 mu_wang <- mean(dat$Wang_Start_NN, na.rm = T)
+mu_sri <- mean(dat$lsri_startNN, na.rm = T)
 
 # Unscale and convert to km if distance
-rss_id_NNdist$NN_Distance_unsc <- (exp(rss_id_NNdist$NN_Distance + mu_NNdist) - 0.125)/1000
-rss_pop_NNdist$NN_Distance_unsc <- (exp(rss_pop_NNdist$NN_Distance + mu_NNdist) - 0.125)/1000
+rss_id_NNdist$NN_Distance_unsc <- exp(rss_id_NNdist$NN_Distance + mu_NNdist) - 0.125
+rss_pop_NNdist$NN_Distance_unsc <- exp(rss_pop_NNdist$NN_Distance + mu_NNdist) - 0.125
 rss_pop_wang$Relatedness_unsc <- rss_pop_wang$Relatedness + mu_wang
 rss_id_wang$Relatedness_unsc <- rss_id_wang$Relatedness + mu_wang
-
+rss_id_sri$SRI_unsc <- exp(rss_id_sri$SRI + mu_sri) - 0.125
+rss_pop_sri$SRI_unsc <- exp(rss_pop_sri$SRI + mu_sri) - 0.125
 
 ## 3- Plot the RSS ====
 
@@ -49,7 +53,8 @@ ggplot() +
         axis.line = element_line(linewidth = 0.5),
         axis.title.x = element_text(size = 18, colour = 'black', vjust = -5),
         axis.title.y = element_text(size = 18, colour = 'black', vjust = 5)) +
-  labs(x = 'Distance to nearest neighbour (km)', y = 'log relative selection strength')
+  labs(x = 'Distance to nearest neighbour (m)', y = 'log relative selection strength') +
+  xlim(0, 1000)
 
 # Relatedness
 ggplot() +
@@ -69,3 +74,23 @@ ggplot() +
         axis.title.x = element_text(size = 18, colour = 'black', vjust = -5),
         axis.title.y = element_text(size = 18, colour = 'black', vjust = 5)) +
   labs(x = 'Relatedness', y = 'log relative selection strength')
+
+# SRI
+ggplot() +
+  geom_line(data = rss_id_sri, 
+            aes(x = SRI_unsc, y = logRSS, group = ANIMAL_ID),
+            linewidth = 0.25, colour = 'mediumblue') +
+  geom_line(data = rss_pop_sri, 
+            aes(x = SRI_unsc, y = logRSS),
+            linewidth = 1, colour = 'mediumblue') +
+  theme(panel.background = element_rect(colour = 'white', fill = 'white'),
+        panel.grid = element_blank(),
+        plot.margin = unit(c(0.25, 0.25, 1, 1), 'cm'),
+        axis.text = element_text(size = 18, colour = 'black'),
+        legend.text = element_text(size = 15, colour = 'black'),
+        legend.title = element_text(size = 18, colour = 'black'),
+        axis.line = element_line(linewidth = 0.5),
+        axis.title.x = element_text(size = 18, colour = 'black', vjust = -5),
+        axis.title.y = element_text(size = 18, colour = 'black', vjust = 5)) +
+  labs(x = 'Simple ratio index', y = 'log relative selection strength') +
+  xlim(0, 0.3) + ylim(0, 40)
