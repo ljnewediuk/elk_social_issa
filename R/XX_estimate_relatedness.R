b6705b94a10f8d52b5cc0436b6aad13106a1e082
ljnewediuk <40477861+ljnewediuk@users.noreply.github.com>
2026-07-13 11:32:28 -0600

    add new related pairs estimates

--- R/XX_estimate_relatedness.R
+++ R/XX_estimate_relatedness.R
@@ -23,3 +23,38 @@ results <- Demerelate::Demerelate(inputdata = microsats, object = TRUE, value = 
 
 # Save Demerelate results (pair-wise relatedness estimates come from results$Empirical_Relatedness)
 saveRDS(results, 'output/Wang_relatedness.rds')
+
+# Convert relatedness into data frame
+wang <- enframe(results$Empirical_Relatedness$Vita) %>%
+  rename(pair = name)
+
+# Make reverse data frame for alternate matches
+wang_reverse <- wang %>%
+  mutate(pair = paste0("VE_", substr(pair, 10, 11), "_VE_", substr(pair, 4, 5)))
+
+# Bind together
+wang_full <- wang %>% bind_rows(wang_reverse)
+
+# Load data
+DT <- readRDS("output/ZOE/5-rdm-locs-sri-NN-N10.RDS") %>%
+  # Add columns to join with pairs
+  mutate(pair_end = paste0("VE_", substr(IDYr, 6, 7), "_VE_", substr(EndNN_ID, 6, 7)),
+         pair_start = paste0("VE_", substr(IDYr, 6, 7), "_VE_", substr(StartNN_ID, 6, 7)))
+
+# Join end pairs
+end_pairs <- wang_full %>%
+  rename(pair_end = pair,
+         Wang_End_NN_corrected = value) %>%
+  right_join(DT)
+
+# Join start pairs
+start_pairs <- wang_full %>%
+  rename(pair_start = pair,
+         Wang_Start_NN_corrected = value) %>% 
+  right_join(end_pairs) %>%
+  # Remove pairs columns
+  select(! c(pair_start, pair_end))
+
+# Save updated data for model
+saveRDS(start_pairs, "output/corrected_iSSA_data.rds")
+
