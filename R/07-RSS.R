69c9f92c5c943d57fb16eb15c200327efb22b9d5
ljnewediuk <j.newedi@gmail.com>
2026-09-24 11:05:18 -0600

    add relative selection exampples

--- R/07-RSS.R
+++ R/07-RSS.R
@@ -1,6 +1,12 @@
 
 ### 07 - RSS for open across social gradient ====
 
+## 1- Prep workspace ====
+
+# Load libraries
+libs <- c('tidyverse')
+lapply(libs, require, character.only = TRUE)
+
 # Load models
 issa_prox_d <- readRDS('models/issa_prox_d.rds')
 issa_sri_d <- readRDS('models/issa_sri_d.rds')
@@ -94,3 +100,28 @@ prox_rss <- get_rss(issa_prox_d, DT, "lStartDist")
 saveRDS(sri_rss, 'models/rss_sri.rds')
 saveRDS(wang_rss, 'models/rss_wang.rds')
 saveRDS(prox_rss, 'models/rss_prox.rds')
+
+# Calculate the relative effects for reporting in results
+
+# Relative selection for open at 75th vs. 25th quantile SRI
+q_sri <- quantile(DT$lsri_startNN, probs = c(0.25, 0.75))
+
+beta_open <- fixef(issa_sri_d)$cond["Open_end"]
+beta_intxn  <- fixef(issa_sri_d)$cond["Open_end:lsri_startNN"]
+
+exp(beta_open + beta_intxn * q_sri)[2]/exp(beta_open + beta_intxn * q_sri)[1]
+
+# Relative selection for open when 0 vs. 500 m from NN
+beta_open <- fixef(issa_prox_d)$cond["Open_end"]
+beta_intxn  <- fixef(issa_prox_d)$cond["Open_end:lStartDist"]
+
+exp(beta_open + beta_intxn * log(5))/exp(beta_open + beta_intxn * log(500))
+
+# Relative selection for open at 75th vs. 25th quantile genetic relatedness
+q_sri <- quantile(DT$Wang_Start_NN, probs = c(0.25, 0.75))
+
+beta_open <- fixef(issa_wang_d)$cond["Open_end"]
+beta_intxn  <- fixef(issa_wang_d)$cond["Open_end:Wang_Start_NN"]
+
+exp(beta_open + beta_intxn * q_sri)[2]/exp(beta_open + beta_intxn * q_sri)[1]
+
